24a07e795ad18256f0f0638cab7b4c0976222d07
Ryan Andriarihanta <hei.tiasoa@gmail.com>
2026-08-20 08:05:16 +0300

    Initial commit

---
+++ src/main/java/org/example/videcoding/VideCodingApplication.java
@@ -0,0 +1,13 @@
+package org.example.videcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/videcoding/VideCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.videcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
